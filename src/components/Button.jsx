d7163b8d452e72295efc24a339a7eb279543906c
jelsinge prajwal <jelsinge.prajwal@gmail.com>
2023-03-31 00:14:20 +0530

    Refractoring

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,6 +5,7 @@ function Button({ text, className, link }) {
     <div>
       <a
         href={link}
+        target="_blank"
         className={`flex justify-center gap-2 items-center font-semibold rounded-[20px] h-[60px]  bg-blue-600 px-[30px] min-w-[152px] ${className} cursor-pointer`}
       >
         <span className="text-base">{text} </span>
--- src/components/ToolsSection.jsx
+++ src/components/ToolsSection.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import entries from "../data/entries";
+import ToolCard from "./ToolCard";
 function ToolsSection() {
     const [search, setSearch] = useState("");
     console.log(entries);
@@ -25,40 +26,7 @@ function ToolsSection() {
                             : entry.title.toLocaleLowerCase().includes(search);
                     })
                     .map((entry, index) => (
-                        <div
-                            key={index}
-                            className="w-full border min-w-[315px] border-pink-600 pt-10 px-4 pb-4 rounded-md"
-                        >
-                            <div>
-                                <h3 className="text-[28px] font-semibold">{entry.title}</h3>
-                                <p className="text-base text-grayFill">{entry.description}</p>
-                            </div>
-                            <div className="flex justify-between items-end mt-4">
-                                <span className="font-mono">{entry.tag}</span>
-                                <div className="flex gap-2 ">
-                                    <div className=" py-2 px-4 rounded-3xl h-[36px] flex items-center bg-bgGray font-semibold cursor-pointer">
-                                        <a
-                                            target={"_blank"}
-                                            href={entry.link !== "" ? entry.link : "/"}
-                                        >
-                                            Source
-                                        </a>
-                                    </div>
-                                    <div className=" py-2 px-4 rounded-3xl h-[36px] flex items-center bg-bgGray font-semibold cursor-pointer">
-                                        <a
-                                            target={"_blank"}
-                                            href={
-                                                entry.github !== ""
-                                                    ? "https://www.github.com/" + entry.github
-                                                    : "/"
-                                            }
-                                        >
-                                            Github
-                                        </a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                        <ToolCard entry={entry} key={index} />
                     ))}
             </div>
         </div>
